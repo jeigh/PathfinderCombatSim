3f2342adf2edeb1e6fa726793008a4d6462bea27
Jason <j-folkens@hotmail.com>
2024-04-26 12:26:29 -0400

    experimental classes that I might come back to later

---
+++ PathfinderCombatSimulator.ClassLibrary/FieldManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Drawing;
+
+// not presently used...   I may re-visit
+namespace PathfinderCombatSimulator
+{
+    public class FieldManager
+    {
+        public void PlaceTeams(List<CombatTeam> combatGroups, FlatTwoDimensionalBattlefield flatTwoDimensionalBattlefield)
+        {
+            List<Point> teamCenters = new List<Point>();
+            if (combatGroups.Count <= 1) return;  
+            if (combatGroups.Count == 2)
+            {
+                teamCenters.Add(new Point(-5, 0));
+                teamCenters.Add(new Point(5, 0));
+            }
+            if (combatGroups.Count == 3)
+            {
+                teamCenters.Add(new Point(6, 0));
+                teamCenters.Add(new Point(-3, 5));
+                teamCenters.Add(new Point(-3, -5));
+            }
+            else
+                throw new NotImplementedException("Maximum of 3 teams are supported.");
+        }
+    }
+}
---
+++ PathfinderCombatSimulator.ClassLibrary/FlatTwoDimensionalBattlefield.cs
@@ -0,0 +1,14 @@
+﻿using System.Drawing;
+
+
+// not yet used.  I may re-visit
+namespace PathfinderCombatSimulator
+{
+    public class FlatTwoDimensionalBattlefield
+    {
+        public Point Center { get; set; } = new System.Drawing.Point(0, 0);
+        public int DefaultStartingDistanceApart { get; set; } = 10;
+    }
+
+
+}
